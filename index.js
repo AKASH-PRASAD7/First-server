81a6603d4f8c0b1c5e5d53459209ea77c1afb3ef
AKASH-PRASAD7 <akash.csel016@bppimt.ac.in>
2022-10-17 12:35:49 +0530

    Added Todo Route

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+const { Console } = require("console");
 const http = require("http");
 
-const port = 8081;
+const port = 1234;
+const arr = ["Cricket", "Football", "Badminton"];
 
 http
-    .createServer((request, response) => {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write("<h1>Created My First Server</h1>");
-        response.end();
-    })
-    .listen(port, () => {
-        console.log(`Server Started at port ${port}`);
-
-    });
-
+  .createServer((request, response) => {
+    const { method, url } = request;
+    if (url === "/") {
+      response.writeHead(200, { "Content-type": "text/html" });
+      response.write("<h1>Go to /todo route</h1>");
+    } else if (url === "/todo") {
+      if (method === "GET") {
+        response.writeHead(200);
+        response.write(arr.toString());
+      } else if (method === "POST") {
+        let dataset = "";
+        request
+          .on("error", (err) => {
+            console.error(err);
+          })
+          .on("data", (chunk) => {
+            dataset += chunk;
+            console.log(chunk);
+          })
+          .on("end", () => {
+            //dataset = JSON.parse(dataset); //converts to JSON FOrmat
+            console.log(dataset);
+          });
+      } else {
+        response.writeHead(501);
+      }
+    } else {
+      response.writeHead(404);
+    }
+    response.end();
+  })
+  .listen(port, () => {
+    console.log(`Server Started at port ${port}`);
+  });
